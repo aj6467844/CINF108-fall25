0a8e0189f734b494735a3176e909e0595b3d3256
aj6467844 <stroadify@proton.me>
2025-08-30 19:13:59 -0400

    Add files via upload

---
+++ assignment-1.py
@@ -0,0 +1,31 @@
+print('''Grading:
+This course is A-E graded and the grades are determined based on graded assignments:
+Weekly assignments count for ~80% of the final grade. A new graded assignment will be issued
+every week on Brightspace. Refer to grading criteria below.
+Midterm and Final Project count for ~20% of the final grade.
+Your final grade will be based on a scale of 100 points:
+A A- B+ B B- C+ C C- D+ D D- E
+100-94 93-89 88-85 84-82 81-79 78-76 75-73 72-70 69-67 66-64 63-60 59-0
+Deliverables and grading:
+Grading criteria for all deliverables counting toward your final grade:
+1. Submitted on time. Any assignment marked as late in Brightspace will have 0 points. Let’s
+be specific: Let’s say the submission deadline on Brightspace is 11:59PM, if your
+submission is timestamped as 11:59:01, Brightspace will mark it as late. Nothing I can do.
+If Brightspace says your submission is late, it is late. No ifs, no buts, no discussion. Please
+plan accordingly.
+2. Quality. In this course, I will be running the code you write. If I can’t successfully run
+your code, I will deduct points.
+3. Content. Please read the assignments carefully and make sure you comply with all the
+requirements listed in the assignment. Incomplete assignments will have points deducted.
+4. You MUST submit ensure that the code repository you are using is publicly accessible. I
+have provided instructions on how to create code repositories in GitHub for this class. If
+you want to use a different repository, you still must ensure it is accessible and that your
+source files are downloadable.
+5. In some instances, you will be required to submit a screenshot of the results of your
+program. Take pictures if needed and submit the .jpeg files.
+6. All grades are individual.
+Missed Exams and Assignments: There are no make-up opportunities for missed assignments,
+quizzes, and exams, although exceptions are made for religious observance or a medical absence
+with a doctor’s note. All class assignments are due on a specific date. Work that is submitted after
+the due date will be awarded 0 points. Read more in the University’s Medical Excuse Policy.
+''')
